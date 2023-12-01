8482de4aee6a5c20c7715ae1ce792e56baf07c4e
Maxime Dupierreux <maxime.dupierreux@gmail.com>
2023-12-01 10:19:53 +0100

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
+private val validNumbers = mapOf(
+    "one" to "one1one",
+    "two" to "two2two",
+    "three" to "three3three",
+    "four" to "four4four",
+    "five" to "five5five",
+    "six" to "six6six",
+    "seven" to "seven7seven",
+    "eight" to "eight8eight",
+    "nine" to "nine9nine",
+    )
+
 fun main() {
+
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val allNumbers = input.map {
+            calibrationRaw -> calibrationRaw.filter { it.isDigit() }
+        }.map { if (it.length == 1) it + it else it[0] + it.last().toString()  }
+
+       return allNumbers.sumOf { it.toInt() }
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val linesWithNumbers = input.map { line ->
+            var fixedLine = line
+            validNumbers.forEach { (key, value) ->
+                fixedLine = fixedLine.replace(key, value)
+            }
+            fixedLine
+        }
+
+        return part1(linesWithNumbers)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_input")
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(testInput).println()
+    part2(testInput).println()
 }
