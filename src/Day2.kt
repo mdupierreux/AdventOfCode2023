32548a9c311b53dfe61fb7960392598cd17953fa
Maxime DUPIERREUX <maxime.dupierreux@neopixl.com>
2023-12-02 14:36:20 +0100

    Solved day2 part1

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,10 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        input.forEach { game ->
-            val gameId = game.substringBefore(":").substringAfter("Game ")
-            val reveal = gameId
+        val matchs = input.mapIndexed { _, game ->
+            game.substringAfter(": ")
+                .split("; ")
+                .map { round ->
+                    round.split(", ")
+                        .associate { colorAndCount ->
+                            colorAndCount.split(" ").let { (count, color) -> color to count.toInt() }
+                        }
+                }
         }
-        return input.size
+        return matchs
+            .withIndex()
+            .filter { (_, rounds) ->
+                rounds.all { colorsFoRound ->
+                    colorsFoRound.getOrDefault("red", 0) <= 12
+                    && colorsFoRound.getOrDefault("green", 0) <= 13
+                    && colorsFoRound.getOrDefault("blue", 0) <= 14
+                }
+            }.sumOf { (index) -> index + 1 }
     }
     
     fun part2(input: List<String>): Int {
